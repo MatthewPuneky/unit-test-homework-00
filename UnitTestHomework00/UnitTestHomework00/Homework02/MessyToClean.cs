4dee37458c86a050b7f8b9698f8d06438201b4bc
Matthew Puneky <matthew.puneky@gmail.com>
2019-04-18 09:59:28 -0500

    feat(test): add test cases that require the source code to be filled out

---
+++ UnitTestHomework00/UnitTestHomework00/Homework02/MessyToClean.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestHomework00.Core.Homework02
+{
+    public class MessyAddressInformation
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class CleanAddressInformation
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddres { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+
+    public class MessyListToCleanList
+    {
+        public List<CleanAddressInformation> CleanAddresses(
+            List<MessyAddressInformation> messyAddresses)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
